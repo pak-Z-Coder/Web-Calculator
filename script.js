b401756fe95fd75db2478fa249a670febe9079e8
Muhammad Zeeshan <109790792+pak-Z-Coder@users.noreply.github.com>
2022-07-29 16:49:13 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,21 +2,18 @@ let numbers = document.getElementById("numbers").children;
 let operators = document.getElementById("operators").children;
 let screen = document.getElementById("screen");
 let equalTo = document.getElementById("equal");
-let digits = "";
-let digits1 = "";
+let string = [];
 let operation = "";
-let html = screen.innerHTML;
+let html = screen.innerText;
 
-//Get Digits:
+//Get string:
 Array.from(numbers).forEach((element) => {
   element.addEventListener("click", function () {
     if (String(screen.innerHTML).length >= 774) {
     } else {
-      
-      recent = element.innerHTML;
-      html += element.innerHTML;
-      screen.innerHTML = html;
-      digits = digits.concat(element.innerText);
+      recent = element.innerText;
+      html += element.innerText;
+      screen.innerText = html;
     }
   });
 });
@@ -25,17 +22,13 @@ clr = document.getElementById("clear");
 clr.addEventListener("click", function () {
   screen.innerHTML = "";
   html = "";
-  digits= "";
+  string = "";
 });
 //Backspace:
 clr = document.getElementById("Backspace");
 clr.addEventListener("click", function () {
-  
-  screen.innerHTML = html;
   screen.innerText = screen.innerText.slice(0, -1);
-  html=screen.innerHTML
-  console.log(screen.innerText);
-  digits = screen.innerText.replace(" ", "");
+  html = screen.innerText;
   
 });
 //Get Operator:
@@ -51,41 +44,28 @@ Array.from(operators).forEach((element) => {
           element == document.getElementById("sqrt")
         ) {
           if (operation == "sq") {
-            screen.innerHTML = `square of ${digits}`;
+            screen.innerText = `square of ${html}`;
           } else if (operation == "sqrt") {
-            screen.innerHTML = `sqrt of ${digits}`;
+            screen.innerText = `sqrt of ${html}`;
           }
         } else {
           console.log(operation);
-          html += element.innerHTML;
-          screen.innerHTML = html;
+          html += ` ${element.id} `;
+          screen.innerText = html;
         }
-        if (digits1 == "") {
-          digits1 = digits;
-        }
-        digits = "";
-      }
     }
-  });
+  }});
 });
 //Operation:
 equalTo.addEventListener("click", function () {
-  if (operation == "+") {
-    screen.innerHTML = `${Number(digits1) + Number(digits)} `;
-  } else if (operation == "-") {
-    screen.innerHTML = `${Number(digits1) - Number(digits)} `;
-  } else if (operation == "*") {
-    screen.innerHTML = `${Number(digits1) * Number(digits)} `;
-  } else if (operation == "/") {
-    screen.innerHTML = `${Number(digits1) / Number(digits)}`;
-  } else if (operation == "sq") {
-    screen.innerText = `${Number(digits1) * Number(digits1)}`;
-    console.log(operation);
+
+  if (operation == "sq") {
+    screen.innerText = `${Number(html) * Number(html)}`;
   } else if (operation == "sqrt") {
-    console.log(digits1);
-    screen.innerText = `${Math.sqrt(Number(digits1))}`;
-    console.log(operation);
+    screen.innerText = `${Math.sqrt(Number(html))}`;
   }
-  digits = "";
-  digits1 = "";
+  else{
+    string=screen.innerText;
+    screen.innerText = `${eval(string)}`;
+  }
 });
